2e93e45cdff7cb719f8403ff114ca2095f9ab1cc
nivalf <flavinleejohn@gmail.com>
2023-02-05 22:55:46 +0000

    Use LED to indicate if the runtime exceeds timeBudget

--- emg_sensors/EMG/EMG.ino
+++ emg_sensors/EMG/EMG.ino
@@ -2,16 +2,17 @@
 #include "EMGFilters.h"
 #include "ExportCSV.h"
 
-#define sensor1Pin A0
-#define sensor2Pin A1
+#define sensor1Pin A1
+#define sensor2Pin A2
+#define sensor3Pin A3
 
-#define SENSOR_COUNT 2
+#define errorLED 2
+
+// Modify value according to number of sensors used
+#define SENSOR_COUNT 3
 
 // Set 0 if Timing o/p need not be printed
 #define TIMING_DEBUG 0
-
-// Modify value according to number of sensors used
-#define SENSORS_COUNT 2
 
 unsigned long runTime;
 unsigned long timeBudget;
@@ -19,27 +20,27 @@ unsigned long timeBudget;
 // discrete filters must works with fixed sample frequence
 // our emg filter only support "SAMPLE_FREQ_500HZ" or "SAMPLE_FREQ_1000HZ"
 // other sampleRate inputs will bypass all the EMG_FILTER
-int sampleRate = SAMPLE_FREQ_1000HZ;
+int sampleRate = SAMPLE_FREQ_500HZ;
 
-EMG_Sensor emg[2] = { EMG_Sensor(sensor1Pin, sampleRate), EMG_Sensor(sensor2Pin, sampleRate) };
+EMG_Sensor emg[SENSOR_COUNT] = { EMG_Sensor(sensor1Pin, sampleRate), EMG_Sensor(sensor2Pin, sampleRate), EMG_Sensor(sensor3Pin, sampleRate) };
 
 ExportCSV myCSV;
 
 void setup() {
   // open serial
-  Serial.begin(115200);
+  Serial.begin(500000);
 
-  // setup for time cost measure
-  // using micros()
-  timeBudget = 1e6 / sampleRate;
+  pinMode(errorLED, OUTPUT);
+
+    // setup for time cost measure
+    // using micros()
+    timeBudget = 1e6 / sampleRate;
   // micros will overflow and auto return to zero every 70 minutes
 
-  for (int i = 0; i < SENSOR_COUNT; i++) {
-    emg[i].init();
-  }
+  initialiseSensors();
 
   myCSV.init();
-  myCSV.setNumberofSensors(SENSORS_COUNT);
+  myCSV.setNumberofSensors(SENSOR_COUNT);
   // myCSV.setColHeaders("Bicep, Tricep");
   myCSV.exportCSVColHeaders();
   myCSV.startTimer();
@@ -52,11 +53,7 @@ void loop() {
 
   myCSV.storeCurrentTime();
 
-  for (int i = 0; i < SENSOR_COUNT; i++) {
-    int value = emg[i].readSensorData();
-    myCSV.storeSensorData(value);
-  }
-  myCSV.exportDataRow();
+  streamSensorData();
 
   runTime = micros() - runTime;
 
--- emg_sensors/EMG/timing.ino
+++ emg_sensors/EMG/timing.ino
@@ -4,21 +4,25 @@
 void maintainOperatingFrequency() {
   // if runTime less than timeBudget, then you still have (timeBudget - runTime)
   // to do your work
-  if (timeBudget > runTime)
+  if (timeBudget > runTime) {
     delayMicroseconds(timeBudget - runTime);
-  else {
-    // if more than timeBudget, the sample rate need to reduce to
-    // SAMPLE_FREQ_500HZ
-    Serial.print("ERROR: runTime exceeds maximum possible value, thus deviating from the sampleRate.\nrunTime: ");
-    Serial.print(runTime);
-    Serial.print(", timeBudget: ");
-    Serial.println(timeBudget);
-
-    if (sampleRate == SAMPLE_FREQ_1000HZ)
-      Serial.println("Current sampleRate is 1000Hz. Either reduce the runTime or reduce the sampleRate to 500Hz.");
-    else
-      Serial.println("Current sampleRate is 500Hz. Further reduction in sample rete is not possible. Try to reduce the runTime further.");
+    digitalWrite(errorLED, LOW);
+  } else {
+    digitalWrite(errorLED, HIGH);
   }
+  // else {
+  //   // if more than timeBudget, the sample rate need to reduce to
+  //   // SAMPLE_FREQ_500HZ
+  //   Serial.print("ERROR: runTime exceeds maximum possible value, thus deviating from the sampleRate.\nrunTime: ");
+  //   Serial.print(runTime);
+  //   Serial.print(", timeBudget: ");
+  //   Serial.println(timeBudget);
+
+  //   if (sampleRate == SAMPLE_FREQ_1000HZ)
+  //     Serial.println("Current sampleRate is 1000Hz. Either reduce the runTime or reduce the sampleRate to 500Hz.");
+  //   else
+  //     Serial.println("Current sampleRate is 500Hz. Further reduction in sample rete is not possible. Try to reduce the runTime further.");
+  // }
 }
 
 // When TIMING_DEBUG is high, console the timing values.
