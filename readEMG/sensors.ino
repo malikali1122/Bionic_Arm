14cc96d8335072313320573e719a4737a68bb02a
Flavin Lee John <flavinleejohn@gmail.com>
2023-02-10 19:59:24 +0000

    File rename and organisation

---
+++ readEMG/sensors.ino
@@ -0,0 +1,15 @@
+// Initialise the EMG sensors
+void initialiseSensors() {
+  for (int i = 0; i < SENSOR_COUNT; i++) {
+    emg[i].init();
+  }
+}
+
+// Stream the 
+void streamSensorData() {
+  for (int i = 0; i < SENSOR_COUNT; i++) {
+    int value = emg[i].readSensorData();
+    myCSV.storeSensorData(value);
+  }
+  myCSV.exportDataRow();
+}
