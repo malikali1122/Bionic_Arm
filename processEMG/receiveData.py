516bff6461602ca8fa54d417cd2e1e4eb9efe6c6
Flavin Lee John <flavinleejohn@gmail.com>
2023-02-10 20:13:04 +0000

    Receive the data sent from arduino by python code

---
+++ processEMG/receiveData.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import serial
+import time
+import matplotlib.pyplot as plt
+
+# make sure the 'COM#' is set according the Windows Device Manager
+ser = serial.Serial('COM14', 115200, timeout=1)
+time.sleep(2)
+
+data = []
+for i in range(500):
+    line = ser.readline()   # read a byte string
+    if line:
+        string = line.decode()  # convert the byte string to a unicode string
+        num = string
+        # num = int(string) # convert the unicode string to an int
+        print(num)
+        data.append(num) # add int to data list
+ser.close()
+
+# build the plot
+plt.plot(data)
+plt.xlabel('Time')
+plt.ylabel('Potentiometer Reading')
+plt.title('Potentiometer Reading vs. Time')
+plt.show()
