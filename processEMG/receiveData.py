61ba53a765ebc0638cbe543fd8a52813b67cfed6
Flavin Lee John <flavinleejohn@gmail.com>
2023-02-11 00:40:57 +0000

    Receive data in python, discard all received data till the start signal, print the received data, handle unicode decode error as exception, close the port on keyboard interrupt

--- processEMG/receiveData.py
+++ processEMG/receiveData.py
@@ -1,31 +1,52 @@
 #!/usr/bin/env python
 
-import serial
 import time
-import matplotlib.pyplot as plt
+import serial
+import serial.tools.list_ports
 
-# make sure the 'COM#' is set according the Windows Device Manager
-ser = serial.Serial('COM14', 115200, timeout=1)
-time.sleep(2)
+#
+# Find the USB port we are on
+#
+commports = serial.tools.list_ports.comports()  # get possible ports
+numPorts = len(commports)
+if (numPorts == 0):
+    print("No serial ports available\n\n")
+    exit()
+if (numPorts > 1):
+    # Have user pick one
+    portNum = 0
+    for port in commports:
+        print("port number ", portNum)
+        print(port)
+        portNum = portNum+1
+    usePort = int(input('enter port number to use 0-'+str(numPorts-1)+':'))
+else:
+    usePort = 0
 
-print("Starting to read data from Arduino...")
+thePort = commports[usePort][0]
+print('using ', thePort, '\n')
 
-data = []
-for i in range(500):
-    line = ser.readline()   # read a byte string
-    if line:
-        # string = line.decode()  # convert the byte string to a unicode string
-        num = line
-        # num = int(string) # convert the unicode string to an int
-        print(num)
-        data.append(num) # add int to data list
-ser.close()
+# open serial port
+device = serial.Serial(thePort, 115200, timeout=1)
 
-print("Finished reading data from Arduino...")
+# wait for arduino to be ready
+# Ignore all previous data read
+start_signal = "<Arduino is ready>".encode()
+device.read_until(start_signal)
+print("Reading Data from Arduino...")
 
-# build the plot
-plt.plot(data)
-plt.xlabel('Time')
-plt.ylabel('Potentiometer Reading')
-plt.title('Potentiometer Reading vs. Time')
-# plt.show()
+while (True):
+    if device.isOpen():
+
+        try:
+            input_data = device.readline()
+            s1 = input_data.strip().decode("utf-8")
+            print(s1)
+        except UnicodeDecodeError as e:
+            print("ERROR")
+            print(e)
+            print(input_data)
+        except KeyboardInterrupt:
+            print("\nStopping read from Arduino...")
+            device.close()
+            exit(0)
